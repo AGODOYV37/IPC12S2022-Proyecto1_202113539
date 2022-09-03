f63bf59674edd02246137ecdebeff6a3636d7fa2
Angel Andres Godoy Valdez <agodoyvaldez@gmail.com>
2022-09-02 21:51:33 -0600

    versión completa

---
+++ src/LoginFrame.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginFrame extends JFrame{
+    Frames frameIn = new Frames();
+    public JPanel loginPanel;
+    private JPanel nLoginPanel;
+    private JLabel encabezadoLogin;
+    private JPanel nwLoginPanel;
+    private JPasswordField textPassWord;
+    private JTextField textUsuario;
+    private JButton ingresarBtn;
+    private JButton regresarBtn;
+
+    public void setRegresarBtn(JButton regresarBtn) {
+        this.setFocusable(false);
+    }
+    public void setIngresarBtn(JButton ingresarBtn) {
+        this.setFocusable(false);
+    }
+
+    public LoginFrame (String title){
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(loginPanel);
+        ingresarBtn.setFocusable(false);
+        regresarBtn.setFocusable(false);
+        ingresarBtn.setBorder(BorderFactory.createMatteBorder(2,2,2,2, Color.black));
+        regresarBtn.setBorder(BorderFactory.createMatteBorder(2,2,2,2,Color.black));
+
+
+        ingresarBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(textUsuario.getText().equals("administrador") && textPassWord.getText().equals("202113539")){
+                    //LoginExitoso
+                    frameIn.PantallaCharacter();
+                    dispose();
+                }
+                else{
+                    JOptionPane.showMessageDialog(null, "Datos incorrectos!", "Error", JOptionPane.ERROR_MESSAGE);
+                    textUsuario.setText(null);
+                    textPassWord.setText(null);
+
+                }
+
+            }
+        });
+        regresarBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textUsuario.setText(null);
+                textPassWord.setText(null);
+                frameIn.PantallaInicio();
+                dispose();
+
+            }
+        });
+    }
+
+
+
+
+
+
+
+
+}
---
+++ src/MainFrame.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainFrame extends JFrame{
+
+    Frames frameIn = new Frames();
+    public JPanel homePanel;
+    public JLabel encabezadoH;
+    public JPanel homeL;
+    public JPanel homeR;
+    public JLabel userPic;
+    public JButton loginBtn;
+
+    public void setLoginBtn(JButton loginBtn) {
+        this.setFocusable(false);
+    }
+
+
+
+
+    public JButton aboutBtn;
+
+    public void setAboutBtn(JButton aboutBtn) {
+        this.setFocusable(false);
+    }
+
+    public MainFrame (String title){
+    super(title);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setContentPane(homePanel);
+    aboutBtn.setFocusable(false);
+    loginBtn.setFocusable(false);
+    aboutBtn.setBorder(BorderFactory.createMatteBorder(2,2,2,2,Color.black));
+    loginBtn.setBorder(BorderFactory.createMatteBorder(2,2,2,2,Color.black));
+
+
+        aboutBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Ángel Andrés Godoy Valdéz, 202113539 ", "About", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+        loginBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frameIn.PantallaLogin();
+                dispose();
+            }
+        });
+    }
+
+
+}
